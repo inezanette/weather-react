8934d3779e11eb1011a6d5c2867a64301b0d3cb4
Inez Hernandez <78990878+inezanette@users.noreply.github.com>
2021-05-13 06:15:39 -0500

    included weather component in Index js

--- src/index.js
+++ src/index.js
@@ -5,4 +5,4 @@ import "./Weather.css";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement); 
+ReactDOM.render(<App />,<Weather />, rootElement); 
