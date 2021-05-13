ca050fde6abde79800e5d8fb20f83eb1b9a406fe
Inez Hernandez <78990878+inezanette@users.noreply.github.com>
2021-05-13 06:20:46 -0500

    removed weather component from index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Weather from "./Weather";
 import "./Weather.css";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />,<Weather />, rootElement); 
+ReactDOM.render(<App />, rootElement); 
